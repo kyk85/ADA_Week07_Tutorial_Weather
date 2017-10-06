68ab6e936283f145319639266f784c9d0494f948
Khoo Yit Keng <30973917+kyk85@users.noreply.github.com>
2017-10-06 22:16:19 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var weatherArray=[]
+
+$(function(){
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position){
+		//alert("Latitude: " + position.coords.latitude + " " + "Longitude: " + position.coords.longitude);
+		var url="https://api.openweathermap.org/data/2.5/forecast/daily?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&cnt=16&appid=8131be7e3e6b2014b3af931e011bd730";
+
+			$.getJSON(url,function(data,status) {
+				console.log(data.city.name);
+				$("#location").html(data.city.name)
+				$.each(data.list, function(index,value){
+					//console.log(index)
+					$("#weatherTableBody").append("<tr><td>"
+						+new Date(value.dt*1000)
+						+"</td><td>"
+						+"<img src='http://openweathermap.org/img/w/"+value.weather[0].icon+".png'>"+"</img></td><td>"
+						+value.temp.day+"&#8451"
+						+"</td><td>"
+						+value.pressure
+						+"</td><td>"
+						+value.humidity
+						+"</td><td>"
+						+"<button onclick='saveItem("+index+")'>Save This</button>"
+						+"</td>"
+						)
+					
+				})
+			})
+		})
+	}
+	
+})				
+
+
+function saveItem(item) {
+		console.log(item)
+	
+
+
+}
